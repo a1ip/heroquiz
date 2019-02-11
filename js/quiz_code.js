1b2341e01ea411f955a5ffc6886c0070077062dc
Philippe Rigovanov <phil@rigovanov.ru>
2019-02-11 14:30:59 +0300

    Add files via upload

---
+++ js/quiz_code.js
@@ -0,0 +1,61 @@
+window.onload = function () {
+    let result = {};
+    let step = 0;
+    function showQuestion(questionNumber) {
+        document.querySelector('.question').innerHTML = quiz[step]['q'];
+        let answer = '';
+        for (let key in quiz[step]['a']) {
+            answer += `<li data-v='${key}' class="answer-variant">${quiz[step]['a'][key]}</li>`;
+        }
+        document.querySelector('.answer').innerHTML = answer;
+
+    }
+
+    document.onclick = function (event) {
+        event.stopPropagation();
+        if (event.target.classList.contains('answer-variant') && step < quiz.length) {
+            // event.target.data
+            if (result[event.target.dataset.v] != undefined) {
+                result[event.target.dataset.v]++;
+            }
+            else {
+                result[event.target.dataset.v] = 0;
+            }
+            step++;
+            if (step == quiz.length) {
+                document.querySelector('.question').remove();
+                document.querySelector('.answer').remove();
+                showResult();
+            }
+            else {
+                showQuestion();
+            }
+        }
+        if (event.target.classList.contains('reload-button')) {
+            location.reload();
+        }
+    }
+
+    function showResult() {
+        const key = Object.keys(result).reduce(function (a, b) { return result[a] > result[b] ? a : b });
+
+        const div = document.createElement('div');
+        div.classList.add('result');
+        div.innerHTML = answers[key]['description'];
+        document.querySelector('main').appendChild(div);
+
+        const img = document.createElement('img');
+        img.src = 'images/' + answers[key]['image'];
+        img.classList.add('result-img')
+        document.querySelector('main').appendChild(img);
+
+        const reloadButton = document.createElement('button');
+        reloadButton.innerHTML = 'New quiz';
+        reloadButton.classList.add('reload-button');
+        document.querySelector('main').appendChild(reloadButton);
+    }
+
+    showQuestion();
+}
+
+
